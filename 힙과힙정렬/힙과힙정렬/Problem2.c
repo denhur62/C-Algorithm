6b5441f614fc74ccb943d48b06ac640ed7ff3bb1
denhur62 <63595768+denhur62@users.noreply.github.com>
2020-08-23 00:05:02 +0900

    3Week Problem Commit

---
+++ 힙과힙정렬/힙과힙정렬/Problem2.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+int H[100];
+int N = 0;
+void swap(int *a, int *b) {
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+void downHeap(int i) {
+
+	int index = i, child;
+	while (index <= N / 2) {
+		child = index*2;
+		if (child + 1 <= N && H[child] < H[child + 1]) {
+			child++;
+		}
+		if (H[child] > H[index]) {
+			swap(&H[child], &H[index]);
+			index = child;
+		}
+		else {
+			break;
+		}
+
+	}
+}
+void rBuildHeap(int index) {
+	if (index > N)
+		return;
+	rBuildHeap(2 * index);
+	rBuildHeap(2 * index + 1);
+	downHeap(index);
+	
+}
+void printHeap() {
+	for (int i = 1; i <= N; i++)
+		printf(" %d", H[i]);
+	printf("\n");
+}
+void buildHeap() {
+
+	for (int i = N / 2; i >= 1; i--)
+		downHeap(i);
+}
+int main() {
+	int num;
+	scanf("%d", &num);
+	N = num;
+	for (int i = 1; i <= num; i++) {
+		scanf("%d",&H[i]);
+	}
+	if (num % 2 == 0)
+		rBuildHeap(1);
+	else
+		buildHeap();
+	printHeap();
+}
